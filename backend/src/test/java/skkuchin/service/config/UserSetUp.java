9eddf8c130535926d268a43d7f46374f59266238
syj0396 <syj0396@naver.com>
2022-12-30 03:41:11 +0900

    refactor(AppUser): major type 변경
    -> 테스트 코드 내 major- type에 맞게 수정
    
    + saveUser 제거
    + userServiceTest의 saveUser_성공()- emailService 관련 코드 무시

--- backend/src/test/java/skkuchin/service/config/UserSetUp.java
+++ backend/src/test/java/skkuchin/service/config/UserSetUp.java
@@ -3,6 +3,7 @@ package skkuchin.service.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import skkuchin.service.domain.User.AppUser;
+import skkuchin.service.domain.User.Major;
 import skkuchin.service.domain.User.Mbti;
 import skkuchin.service.domain.User.Role;
 import skkuchin.service.repo.UserRepo;
@@ -16,7 +17,7 @@ public class UserSetUp {
     private UserRepo userRepo;
 
 
-    public Long saveUser(String nickname, String username, String password, String email, String student_id, String major, String image, Mbti mbti, Collection<Role> roles) {
+    public Long saveUser(String nickname, String username, String password, String email, String student_id, Major major, String image, Mbti mbti, Collection<Role> roles) {
         AppUser user = AppUser.builder()
                 .nickname(nickname)
                 .username(username)
