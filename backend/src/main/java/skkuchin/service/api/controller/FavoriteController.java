475a1ed6f72574fff4e14a026521d9c347e7d576
강병준 <freudbjk@gmail.com>
2023-01-12 13:19:03 +0900

    perf(즐겨찾기): 다른 사람의 즐겨찾기 기록 삭제 방지하는 기능 추가

--- backend/src/main/java/skkuchin/service/api/controller/FavoriteController.java
+++ backend/src/main/java/skkuchin/service/api/controller/FavoriteController.java
@@ -13,6 +13,7 @@ import skkuchin.service.api.dto.ReviewDto;
 import skkuchin.service.domain.User.AppUser;
 import skkuchin.service.domain.Map.Favorite;
 import skkuchin.service.domain.Map.Place;
+import skkuchin.service.repo.FavoriteRepo;
 import skkuchin.service.repo.UserRepo;
 import skkuchin.service.security.auth.PrincipalDetails;
 import skkuchin.service.service.FavoriteService;
@@ -26,6 +27,7 @@ import java.util.List;
 public class FavoriteController {
     private final FavoriteService favoriteService;
     private final UserRepo userRepo;
+    private final FavoriteRepo favoriteRepo;
     @PostMapping("")
     @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
     public ResponseEntity<?> write(@Valid @RequestBody FavoriteDto.PostRequest dto, @AuthenticationPrincipal PrincipalDetails principalDetails) {
@@ -52,8 +54,8 @@ public class FavoriteController {
     @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
     public ResponseEntity<?> delete(@PathVariable Long favoriteId, @AuthenticationPrincipal PrincipalDetails principalDetails) {
         AppUser user = principalDetails.getUser();
-        //AppUser user = userRepo.findByEmail("test@test");
         Long userId = user.getId();
+        //AppUser user = userRepo.findByEmail("test@test");
         favoriteService.delete(favoriteId, userId);
         return new ResponseEntity<>(new CMRespDto<>(1, "즐겨찾기 삭제 완료", null), HttpStatus.OK);
     }
