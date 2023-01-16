76cfe3cd50db231f4cd0181b82b0b1dd95c85ab6
hj213 <0213yhj@naver.com>
2023-01-16 18:48:57 +0900

    hj

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -21,7 +21,7 @@ const Dashboard = () => {
             <div className="p-5 bg-light rounded-3">
                 <div className="container-fluid py-3">
                     <h1 className="display-5 fw-bold">My Dashboard</h1>
-                    <p className="fs-4 mt-3">welcome {user !== null && user.nickname} to 스꾸친!</p>
+                    <p className="fs-4 mt-3">welcome {user !== null && user.nickname} to 스꾸친!  </p>
                 </div>
 
             </div>
--- frontend/src/pages/list.js
+++ frontend/src/pages/list.js
@@ -1,15 +1,19 @@
 import { useDispatch, useSelector } from "react-redux"
+import { load_places } from "../actions/place/place";
 import Layout from "../hocs/Layout";
 
 export default function list(){
 
     const dispatch = useDispatch();
-    dispatch(load_place());
-    // const place = useSelector(state => state.place);
-    // console.log(place);
+    const dispatchPlace = () => dispatch(load_places);
+    const place = useSelector(state => state.place.place);
+    const user = useSelector(state => state.auth.user);
+
 
     return(
        <Layout>
+            {/* {user.map(item => (<div key={item.id}>{item}</div>))} */}
+            {/* {user.nickname} */}
         </Layout>
     )
 }
