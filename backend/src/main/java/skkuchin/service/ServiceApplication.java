f43f95622bf2b9583d85d2552f9fcb76b25fb0da
syj0396 <syj0396@naver.com>
2023-01-19 23:06:51 +0900

    feat: 매칭 API 구현

--- backend/src/main/java/skkuchin/service/ServiceApplication.java
+++ backend/src/main/java/skkuchin/service/ServiceApplication.java
@@ -42,8 +42,8 @@ public class ServiceApplication {
 			//test 계정 생성
 			userService.saveTestUser(new UserDto.SignUpForm("테스트", "test", "12341234", "12341234", "test1@test1", "0000000001", Major.건축학과));
 			//데이터 자동 주입
-			String path = System.getProperty("user.dir") + "\\src\\main\\java\\skkuchin\\service\\data\\";
-			//String path = System.getProperty("user.dir") + "/src/main/java/skkuchin/service/data/"; //Mac 공통 경로
+			//String path = System.getProperty("user.dir") + "\\src\\main\\java\\skkuchin\\service\\data\\";
+			String path = System.getProperty("user.dir") + "/src/main/java/skkuchin/service/data/"; //Mac 공통 경로
 			tagService.insertData(path);
 			placeService.insertData(path);
 			imageService.insertData(path);
