3d306658738103239e9a91b17fad5d9575632fac
hj213 <0213yhj@naver.com>
2023-02-15 03:03:04 +0900

    feat(chatTime): 약속 장소 및 시간 정하기 페이지 제작

--- frontend/src/pages/makeProfile.js
+++ frontend/src/pages/makeProfile.js
@@ -305,7 +305,10 @@ export default function makeProfile(){
     const [mbti, setMbti] = useState('');
     const [condition, setCondition] = useState(false); //확인버튼 조건 
 
-    const [visibility, setVisibility] = useState(''); //어느 페이지에서 접근하는지에 따라 상단 바뀌도록
+    const [visibility, setVisibility] = useState({
+        '뒤로가기': 'hidden',
+        '건너뛰기': 'hidden',
+    }); //어느 페이지에서 접근하는지에 따라 상단 바뀌도록
 
     //아이콘 클릭시
     const handleIconOnclick = (event) =>{
