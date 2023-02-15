763bab2965f758ac09c074f7bad21544dc6df54e
hj213 <0213yhj@naver.com>
2023-02-16 01:35:27 +0900

    feat(clickProfile): 프로필 보기 제작

--- frontend/src/pages/chatPlace.js
+++ frontend/src/pages/chatPlace.js
@@ -1,4 +1,4 @@
-import { useEffect,useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
