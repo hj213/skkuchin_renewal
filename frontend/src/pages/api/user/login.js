3408a76ed418e1b126b7295946f5177c71ad5d4a
myungjunlee <dlaudwns789@gmail.com>
2023-02-24 03:48:10 +0900

    test: 서버 테스트

--- frontend/src/pages/api/user/login.js
+++ frontend/src/pages/api/user/login.js
@@ -26,7 +26,7 @@ export default async (req, res) => {
             if (apiRes.status === 200) {
                 res.setHeader('Set-Cookie', [
                     cookie.serialize(
-                        'access', resValue.data.access, {
+                        'access', "123456789", {
                             httpOnly: true,
                             secure: process.env.NODE_ENV === 'production',
                             maxAge: 60 * 30,
@@ -35,7 +35,7 @@ export default async (req, res) => {
                         }
                     ),
                     cookie.serialize(
-                        'refresh', resValue.data.refresh, {
+                        'refresh', "12345678923123", {
                             httpOnly: true,
                             secure: process.env.NODE_ENV === 'production',
                             maxAge: 60 * 60 * 24,
