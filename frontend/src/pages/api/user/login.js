658a8ebf781fca88b19de6e66d81f66e5986a809
myungjunlee <dlaudwns789@gmail.com>
2023-02-24 03:32:16 +0900

    test: 서버 테스트

--- frontend/src/pages/api/user/login.js
+++ frontend/src/pages/api/user/login.js
@@ -1,4 +1,4 @@
-// import cookie from 'cookie';
+import cookie from 'cookie';
 import { API_URL } from '../../../config/index';
 
 
@@ -21,35 +21,35 @@ export default async (req, res) => {
                 body: body
             });
 
-            // const resValue = await apiRes.json();
+            const resValue = await apiRes.json();
 
             if (apiRes.status === 200) {
-                // res.setHeader('Set-Cookie', [
-                //     cookie.serialize(
-                //         'access', resValue.data.access, {
-                //             httpOnly: true,
-                //             secure: process.env.NODE_ENV === 'production',
-                //             maxAge: 60 * 30,
-                //             sameSite: 'strict',
-                //             path: '/api/'
-                //         }
-                //     ),
-                //     cookie.serialize(
-                //         'refresh', resValue.data.refresh, {
-                //             httpOnly: true,
-                //             secure: process.env.NODE_ENV === 'production',
-                //             maxAge: 60 * 60 * 24,
-                //             sameSite: 'strict',
-                //             path: '/api/'
-                //         }
-                //     )
-                // ]);
+                res.setHeader('Set-Cookie', [
+                    cookie.serialize(
+                        'access', resValue.data.access, {
+                            httpOnly: true,
+                            secure: process.env.NODE_ENV === 'production',
+                            maxAge: 60 * 30,
+                            sameSite: 'strict',
+                            path: '/api/'
+                        }
+                    ),
+                    cookie.serialize(
+                        'refresh', resValue.data.refresh, {
+                            httpOnly: true,
+                            secure: process.env.NODE_ENV === 'production',
+                            maxAge: 60 * 60 * 24,
+                            sameSite: 'strict',
+                            path: '/api/'
+                        }
+                    )
+                ]);
                 return res.status(200).json({
-                    success: "성공"
+                    success: resValue.message
                 });
             } else {
                 return res.status(apiRes.status).json({
-                    error: "실패"
+                    error: resValue.message
                 });
             }
         } catch(err) {
