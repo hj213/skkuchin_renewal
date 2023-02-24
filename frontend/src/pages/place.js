c669c4b832a1639b73fcc0644242963d5acc32ea
hj213 <0213yhj@naver.com>
2023-02-24 19:27:34 +0900

    feat(place): 카드 위치 수정

--- frontend/src/pages/place.js
+++ frontend/src/pages/place.js
@@ -103,10 +103,8 @@ const PlacePage = () => {
         event.preventDefault();
 
         const WINDOW_HEIGHT = window.innerHeight;
-        const TARGET_HEIGHT = WINDOW_HEIGHT * 0.56;
-        if(WINDOW_HEIGHT > 1000){
-            TARGET_HEIGHT = WINDOW_HEIGHT*0.58;
-        }
+        const TARGET_HEIGHT = WINDOW_HEIGHT-160;
+        console.log(TARGET_HEIGHT);
         const newHeight = window.innerHeight - event.touches[0].clientY;
         if (newHeight >= preNewHeight) {
             // console.log(newHeight);
