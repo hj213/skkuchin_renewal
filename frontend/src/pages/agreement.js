da70ebef6f07af1b103a005f3221cde997082ea8
syj0396 <syj0396@naver.com>
2023-02-26 23:46:52 +0900

    feat(마이페이지): 약관 및 정책 페이지 제작

--- frontend/src/pages/agreement.js
+++ frontend/src/pages/agreement.js
@@ -13,20 +13,21 @@ import AdAgreement from '../components/MyPage/AdAgreement';
 export default function agreement() {
     const router = useRouter();
 
-    const list = ["약관 및 정책", "이용약관", "개인정보 처리방침", "광고성 정보 수신 동의"]
+    const list = ["이용약관", "개인정보 처리방침", "광고성 정보 수신 동의"]
     const [page, setPage] = useState("약관 및 정책");
 
     const backClick = e => {
-        if (page == list[0]) {
+        if (page == "약관 및 정책") {
             router.push('/myPage');
         } else {
-            setPage(list[0]);
+            setPage("약관 및 정책");
         }
     }
 
     const handleClick = e => {
         console.log(e.target.id);
         setPage(e.target.id);
+        
     }
 
     return (
@@ -35,7 +36,7 @@ export default function agreement() {
 
         <Box
             sx={{
-            margin: '45px 16px 16px 16px',
+            margin: '45px 22px 22px 22px',
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
@@ -44,19 +45,24 @@ export default function agreement() {
         {/* 상단 */}
         <Container style={{padding:'0px', alignItems: 'center', marginBottom: '55px'}}>
                         <Grid container>
-                            <Grid item style={{margin:'0px 0px 0px 20px', visibility:'none'}}>
-                                <Image src={back} width={11} height={18} name='back' onClick={backClick}/>
+                            <Grid item style={{margin:'0px 0px 0px 0px', visibility:'none'}}>
+                                <Image src={back} width={11} height={18} name='back' onClick={backClick} style={{justifySelf: 'start'}} />
                             </Grid>
+                            {page == "약관 및 정책" || "이용약관" ? 
                             <Grid item style={{marginLeft:'31%'}}>
                                 <Typography style={{margin:'0px 0px 0px 0px', textAlign:'center',fontSize:'18px', fontWeight: '700'}}>{page}</Typography>
                             </Grid>
+                            :
+                            <Grid item style={{marginLeft:'26%'}}>
+                                <Typography style={{margin:'0px 0px 0px 0px', textAlign:'center',fontSize:'18px', fontWeight: '700'}}>{page}</Typography>
+                            </Grid>}
                         </Grid>
         </Container>
 
-        {page == list[0] && list.map(elem => 
-            <div style={{display: 'grid', gridTemplateColumns: '1px 11px', marginBottom: '32px'}} id={elem} onClick={handleClick}>
+        {page == "약관 및 정책" && list.map(elem => 
+            <div style={{display: 'grid', gridTemplateColumns: '1fr 38px', marginBottom: '32px', width: '100%'}} id={elem} onClick={handleClick}>
                 <Typography id={elem} style={{fontSize: '16px'}}>{elem}</Typography>
-                <Image width={10.43} height={17.69} src={next} />
+                <Image width={38} height={38} src={next} id={elem}/>
             </div>    
         )}
 
