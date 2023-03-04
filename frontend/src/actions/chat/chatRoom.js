1ae69af96543e414ce82369343fce02d2664a604
myungjunlee <dlaudwns789@gmail.com>
2023-03-04 21:13:18 +0900

    feat(chat): 프론트와 연결

--- frontend/src/actions/chat/chatRoom.js
+++ frontend/src/actions/chat/chatRoom.js
@@ -2,6 +2,20 @@ import Cookies from 'js-cookie';
 import { API_URL } from '../../config';
 import { AUTHENTICATED_FAIL } from '../auth/types';
 import { request_refresh } from '../auth/auth';
+import {
+    CREATE_CHAT_ROOM_SUCCESS,
+    CREATE_CHAT_ROOM_FAIL,
+    REPLY_CHAT_REQUEST_SUCCESS,
+    REPLY_CHAT_REQUEST_FAIL,
+    MODIFY_CHAT_BLOCK_SUCCESS,
+    MODIFY_CHAT_BLOCK_FAIL,
+    MODIFY_CHAT_ALARM_SUCCESS,
+    MODIFY_CHAT_ALARM_FAIL,
+    EXIT_CHAT_ROOM_SUCCESS,
+    EXIT_CHAT_ROOM_FAIL
+}
+    from './types';
+import { EXIT_CHAT_ROOM_FAIL, EXIT_CHAT_ROOM_SUCCESS } from './type';
 
 export const make_room = async (username, callback) => {
     await dispatch(request_refresh());
@@ -32,11 +46,20 @@ export const make_room = async (username, callback) => {
         const apiRes = await res.json();
 
         if (res.status === 201) {
+            dispatch({
+                type: CREATE_CHAT_ROOM_SUCCESS
+            })
             if (callback) callback([true, apiRes.message]);
         } else {
+            dispatch({
+                type: CREATE_CHAT_ROOM_FAIL
+            })
             if (callback) callback([false, apiRes.message]);
         }
     } catch(error) {
+        dispatch({
+            type: CREATE_CHAT_ROOM_FAIL
+        })
         if (callback) callback([false, error]);
     }
 };
@@ -57,7 +80,7 @@ export const reply_chat_request = async (reaction, room_id, callback) => {
     });
 
     try {
-        const res = await fetch(`${API_URL}/api/chat/room/reaction/${room_id}`, {
+        const res = await fetch(`${API_URL}/api/chat/room/requset/${room_id}`, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
@@ -70,11 +93,20 @@ export const reply_chat_request = async (reaction, room_id, callback) => {
         const apiRes = await res.json();
 
         if (res.status === 200) {
+            dispatch({
+                type: REPLY_CHAT_REQUEST_SUCCESS
+            })
             if (callback) callback([true, apiRes.message]);
         } else {
+            dispatch({
+                type: REPLY_CHAT_REQUEST_FAIL
+            })
             if (callback) callback([false, apiRes.message]);
         }
     } catch(error) {
+        dispatch({
+            type: REPLY_CHAT_REQUEST_FAIL
+        })
         if (callback) callback([false, error]);
     }
 };
@@ -108,11 +140,20 @@ export const block_user = async (reaction, room_id, callback) => {
         const apiRes = await res.json();
 
         if (res.status === 200) {
+            dispatch({
+                type: MODIFY_CHAT_BLOCK_SUCCESS
+            })
             if (callback) callback([true, apiRes.message]);
         } else {
+            dispatch({
+                type: MODIFY_CHAT_BLOCK_FAIL
+            })
             if (callback) callback([false, apiRes.message]);
         }
     } catch(error) {
+        dispatch({
+            type: MODIFY_CHAT_BLOCK_FAIL
+        })
         if (callback) callback([false, error]);
     }
 };
@@ -146,11 +187,20 @@ export const set_chat_room_alarm = async (reaction, room_id, callback) => {
         const apiRes = await res.json();
 
         if (res.status === 200) {
+            dispatch({
+                type: MODIFY_CHAT_ALARM_SUCCESS
+            })
             if (callback) callback([true, apiRes.message]);
         } else {
+            dispatch({
+                type: MODIFY_CHAT_ALARM_FAIL
+            })
             if (callback) callback([false, apiRes.message]);
         }
     } catch(error) {
+        dispatch({
+            type: MODIFY_CHAT_ALARM_FAIL
+        })
         if (callback) callback([false, error]);
     }
 };
@@ -177,11 +227,20 @@ export const exit_room = async (room_id, callback) => {
         const apiRes = await res.json();
 
         if (res.status === 200) {
+            dispatch({
+                type: EXIT_CHAT_ROOM_SUCCESS
+            })
             if (callback) callback([true, apiRes.message]);
         } else {
+            dispatch({
+                type: EXIT_CHAT_ROOM_FAIL
+            })
             if (callback) callback([false, apiRes.message]);
         }
     } catch(error) {
+        dispatch({
+            type: EXIT_CHAT_ROOM_FAIL
+        })
         if (callback) callback([false, error]);
     }
 };
