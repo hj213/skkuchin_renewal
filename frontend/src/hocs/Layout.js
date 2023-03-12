7add528a4c23992f4a7a7405d3b37fdd83cd28a6
myungjunlee <dlaudwns789@gmail.com>
2023-03-12 15:21:28 +0900

    refactor: 디버그 모드 해제

--- frontend/src/hocs/Layout.js
+++ frontend/src/hocs/Layout.js
@@ -90,7 +90,7 @@ const Layout = ({title, content, children}) => {
     stompClient = Stomp.over(sockJS);
     stompClient.heartbeat.outgoing = 0;
     stompClient.heartbeat.incoming = 0;
-    // stompClient.debug = null;
+    stompClient.debug = null;
 
     const onError = (e) => {
         connectStompClient();
